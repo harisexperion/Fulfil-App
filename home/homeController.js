b3116c23861ea77e4e99dad5086d179bfd7db290
harisexperion <haris.kk@experionglobal.com>
2017-10-03 22:37:24 +0530

    Search Changed , Code Beautified

--- home/homeController.js
+++ home/homeController.js
@@ -1,32 +1,36 @@
+(function() {
+      function HomeController(menuService, $scope, $state) {
+          var ctrl = this;
+          ctrl.menu = menuService.getMenu();
+          ctrl.activeMenu = "contact";
+  
+          function onStateChange(param) {
+              $state.go('home.list', {
+                  "activeMenu": param
+              });
+              ctrl.activeMenu = param;
+              ctrl.query = '';
+              ctrl.isSearch = false;
+  
+          }
+  
+          function onQueryChange() {
+              ctrl.isSearch = false;
+              onSearch();
+          }
+  
+          function onSearch() {
+              $state.go('home.search', {
+                  "query": ctrl.query
+              });
+              ctrl.isSearch = true;
+          }
 
-(function() { 
-    
-    function HomeController(menuService , $scope , $state ) {
-      
-       var ctrl = this;
-       ctrl.menu = menuService.getMenu();
+          onStateChange("contact");
 
-       ctrl.activeMenu = "contact";
-       onStateChange("contact");
-
-       function onStateChange(param) {
-         $state.go('home.list' , { "activeMenu" :  param});
-         ctrl.activeMenu = param;
-       }
-
-       function onSearch() {
-
-        $state.go('home.search' , { "query" :  ctrl.query});
-       }
-       ctrl.onStateChange = onStateChange;
-       ctrl.onSearch = onSearch;
-
-
-
-
-    }
-    
-    angular.module('myApp').controller('HomeController', HomeController);
-    
-    
-    })();
+          ctrl.onStateChange = onStateChange;
+          ctrl.onQueryChange = onQueryChange;
+          ctrl.onSearch = onSearch;
+      }
+      angular.module('myApp').controller('HomeController', HomeController);
+  })();
--- list/listController.js
+++ list/listController.js
@@ -1,19 +1,8 @@
-
-(function() { 
-    
-    function ListController($stateParams ,  listService) {
-      
-       var ctrl = this;
-       ctrl.param = $stateParams.activeMenu;
-       ctrl.list = listService.getList();
-
-
-
-
-
-    }
-    
-    angular.module('myApp').controller('ListController', ListController);
-    
-    
-    })();
+(function() {
+        function ListController($stateParams, listService) {
+            var ctrl = this;
+            ctrl.param = $stateParams.activeMenu;
+            ctrl.list = listService.getList();
+        }
+        angular.module('myApp').controller('ListController', ListController);
+})();
